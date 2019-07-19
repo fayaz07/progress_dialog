cdddd976777cfb87cd33807daee4f068e73955c2
Bagas Alfiandhi Nugroho <dennbagas@gmail.com>
2019-07-19 18:43:04 +0700

    prevent backbutton pressed during the dialog show

--- lib/progress_dialog.dart
+++ lib/progress_dialog.dart
@@ -10,14 +10,20 @@ double _progress = 0.0;
 bool _isShowing = false;
 
 class ProgressDialog {
-  _MyDialog _dialog;
-
+  /// rename the _MyWidget to _DialogWidget
+  _DialogWidget _dialog;
   BuildContext _buildContext, _context;
 
-  ProgressDialog(
-      BuildContext buildContext, ProgressDialogType progressDialogtype) {
-    _buildContext = buildContext;
+  bool barrierDismissable;
 
+  ProgressDialog(
+    BuildContext buildContext,
+    ProgressDialogType progressDialogtype, {
+
+    /// add this optional argument to determine barrier is dismissable or not
+    this.barrierDismissable,
+  }) {
+    _buildContext = buildContext;
     _progressDialogType = progressDialogtype;
   }
 
@@ -42,39 +48,54 @@ class ProgressDialog {
   }
 
   void hide() {
-    _isShowing = false;
-    Navigator.of(_context).pop();
-    print('ProgressDialog dismissed');
+    if (_isShowing) {
+      _isShowing = false;
+      Navigator.of(_context).pop();
+      debugPrint('ProgressDialog dismissed');
+    }
   }
 
   void show() {
-    _dialog = new _MyDialog();
-    _isShowing = true;
-    print('ProgressDialog shown');
-    showDialog<dynamic>(
-      context: _buildContext,
-      barrierDismissible: false,
-      builder: (BuildContext context) {
-        _context = context;
-        return Dialog(
+    if (!_isShowing) {
+      /// make default value of barrierDismissable is false
+      final _barrierDismissable = barrierDismissable ?? false;
+
+      /// pass the _barrierDismissable to the dialogWidget
+      _dialog = new _DialogWidget(_barrierDismissable);
+      _isShowing = true;
+      debugPrint('ProgressDialog shown');
+      showDialog<dynamic>(
+        context: _buildContext,
+        barrierDismissible: _barrierDismissable,
+        builder: (BuildContext context) {
+          _context = context;
+          return Dialog(
             insetAnimationCurve: Curves.easeInOut,
             insetAnimationDuration: Duration(milliseconds: 100),
             elevation: 10.0,
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(10.0))),
-            child: _dialog);
-      },
-    );
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10.0))),
+            child: _dialog,
+          );
+        },
+      );
+    }
   }
 }
 
 // ignore: must_be_immutable
-class _MyDialog extends StatefulWidget {
-  var _dialog = new _MyDialogState();
+class _DialogWidget extends StatefulWidget {
+  var _dialog = new _DialogWidgetState();
 
   update() {
-    _dialog.changeState();
+    /// update the state only if the dialog is Showing
+    if (_isShowing) {
+      _dialog.changeState();
+    }
   }
+
+  /// add the parameter of barrierDismissable
+  bool barrierDismissable;
+  _DialogWidget(this.barrierDismissable);
 
   @override
   // ignore: must_be_immutable
@@ -83,8 +104,7 @@ class _MyDialog extends StatefulWidget {
   }
 }
 
-class _MyDialogState extends State<_MyDialog> {
-
+class _DialogWidgetState extends State<_DialogWidget> {
   changeState() {
     setState(() {});
   }
@@ -93,59 +113,54 @@ class _MyDialogState extends State<_MyDialog> {
   void dispose() {
     super.dispose();
     _isShowing = false;
-    print('ProgressDialog dismissed by back button');
+    debugPrint('ProgressDialog dismissed by back button');
   }
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-        height: 100.0,
-        child: Row(children: <Widget>[
-          const SizedBox(width: 15.0),
-          SizedBox(
-            width: 60.0,
-            child: Image.asset(
-              'assets/double_ring_loading_io.gif',
-              package: 'progress_dialog',
+    /// add WillPopScope widget to prevent backbutton pressed during the dialog is showing
+    return WillPopScope(
+      /// onWillPop take the barrierDismissable value
+      onWillPop: () => Future.value(widget.barrierDismissable),
+      child: SizedBox(
+          height: 100.0,
+          child: Row(children: <Widget>[
+            const SizedBox(width: 15.0),
+            SizedBox(
+              width: 60.0,
+              child: Image.asset(
+                'assets/double_ring_loading_io.gif',
+                package: 'progress_dialog',
+              ),
             ),
-          ),
-          const SizedBox(width: 15.0),
-          Expanded(
-            child: _progressDialogType == ProgressDialogType.Normal
-                ? Text(_dialogMessage,
-                    textAlign: TextAlign.justify,
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 22.0,
-                        fontWeight: FontWeight.w700))
-                : Stack(
-                    children: <Widget>[
-                      Positioned(
-                        child: Text(_dialogMessage,
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 22.0,
-                                fontWeight: FontWeight.w700)),
-                        top: 35.0,
-                      ),
-                      Positioned(
-                        child: Text("$_progress/100",
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 15.0,
-                                fontWeight: FontWeight.w400)),
-                        bottom: 15.0,
-                        right: 15.0,
-                      ),
-                    ],
-                  ),
-          )
-        ]));
+            const SizedBox(width: 15.0),
+            Expanded(
+              child: _progressDialogType == ProgressDialogType.Normal
+                  ? Text(_dialogMessage,
+                      textAlign: TextAlign.justify,
+                      style: TextStyle(color: Colors.black, fontSize: 22.0, fontWeight: FontWeight.w700))
+                  : Stack(
+                      children: <Widget>[
+                        Positioned(
+                          child: Text(_dialogMessage,
+                              style: TextStyle(color: Colors.black, fontSize: 22.0, fontWeight: FontWeight.w700)),
+                          top: 35.0,
+                        ),
+                        Positioned(
+                          child: Text("$_progress/100",
+                              style: TextStyle(color: Colors.black, fontSize: 15.0, fontWeight: FontWeight.w400)),
+                          bottom: 15.0,
+                          right: 15.0,
+                        ),
+                      ],
+                    ),
+            )
+          ])),
+    );
   }
 }
 
 class MessageBox {
-
   BuildContext buildContext;
   String message = " ", title = " ";
 
